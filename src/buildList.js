c77b3cad7894bc031d6f6009328302024e2998f9
bluffer <melihyuxel@gmail.com>
2024-02-27 15:21:44 +0300

    ff

--- src/buildList.js
+++ src/buildList.js
@@ -1,11 +1,11 @@
 const { version } = require("../package.json");
 require('dotenv').config()
-const metisGoerli = require("./tokens/metis-goerli.json");
+const metisAndromeda = require("./tokens/metis-andromeda.json");
 
 const BASE_URL = process.env.BASE_URL
 
 module.exports = function buildList() {
-  const tokens = [...metisGoerli]
+  const tokens = [...metisAndromeda]
   const processedTokens = JSON.parse(JSON.stringify(tokens).replace(/BASE_URL/g, BASE_URL))
   const parsed = version.split(".");
 
@@ -14,8 +14,8 @@ module.exports = function buildList() {
     timestamp: new Date().toISOString(),
     version: { major: +parsed[0], minor: +parsed[1], patch: +parsed[2], },
     tags: {},
-    logoURI: "https://app.camelot.exchange/images/logo-sm.svg",
-    keywords: ["camelot", "default"],
+    logoURI: "https://app.hercules.exchange/images/logo-sm.svg",
+    keywords: ["hercules", "default", "camelot", "dex"],
     tokens: processedTokens
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
